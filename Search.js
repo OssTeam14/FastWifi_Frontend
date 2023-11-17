c081b6e9b064586a44f8fecd1fa8626dae88a487
aarmia <50602778+aarmia@users.noreply.github.com>
2023-11-17 23:55:58 +0900

    Add files via upload
    Branch merge

---
+++ Search.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+const Search = ({ getValue, setModalState, toggleMenu }) => {
+  return (
+    <div className="title" id='border'>
+      <div className='title_child' id='border'>
+          LOGO
+        </div>
+      <div className='title_search'>
+        <input className='search' onChange={getValue} />
+        <img className='searchname' alt='search' src='img/search.svg' onClick={() => setModalState(true)} />
+      </div>
+      <img className='menubtn' alt='menu' src='img/menu.svg' onClick={toggleMenu}/>
+    </div>
+    
+  );
+}
+export default Search;
+
+//HEADER
